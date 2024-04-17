9df42a40d539afae949f64d5f9b11179890de794
Gabriela de Leon <gldeleon@Gabrielas-MacBook-Pro.local>
2024-04-17 17:26:53 -0400

    beginnings

---
+++ src/Question.java
@@ -0,0 +1,51 @@
+import javax.swing.*;
+import java.util.ArrayList;
+
+class Question {
+    int ID;
+    enum optType {DROP,RADIO,CHECK}
+    optType t;
+    String questionText;
+    ArrayList<String> qOptions;
+    int[] respCounter;
+
+    Question(int ID, int optNum){ //cannot initialize w/o this
+        this.ID=ID;
+        this.qOptions = new ArrayList<String>(optNum);
+        this.respCounter = new int[optNum];
+        this.t = optType.DROP;
+        for(int i=0; i<optNum;i++){
+            respCounter[i]=0;
+        } //start the list of counters at 0
+    }
+    Question(int ID, int optNum, String questionText, optType t){ //completely create q
+        this.ID=ID;
+        this.qOptions = new ArrayList<String>(optNum);
+        this.respCounter = new int[optNum];
+        this.t = t;
+        this.questionText = questionText;
+        for(int i=0; i<optNum;i++){
+            respCounter[i]=0;
+        } //start the list of counters at 0
+    }
+    public optType getT() {
+        return t;
+    }
+    public void setT(optType t) {
+        this.t = t;
+    }
+
+    public String getQuestionText() {
+        return questionText;
+    }
+
+    public void setQuestionText(String questionText) {
+        this.questionText = questionText;
+    }
+    public void addOption(String x){
+        qOptions.add(x);
+    }
+
+}
+
+
---
+++ src/QuestionNode.java
@@ -0,0 +1,31 @@
+import javax.swing.*;
+public class QuestionNode extends JFrame {
+        private Question question;
+        private QuestionNode next;
+        private QuestionNode prev;
+
+        public QuestionNode(Question question) {
+            this.question = question;
+            this.next = null;
+            this.prev = null;
+        }
+        public Question getQuestion() {
+            return question;
+        }
+        public QuestionNode getNext() {
+            return next;
+        }
+        public void setNext(QuestionNode next) {
+            this.next = next;
+        }
+        public QuestionNode getPrev() {
+            return prev;
+        }
+        public void setPrev(QuestionNode prev) {
+            this.prev = prev;
+        }
+
+    public void display(){
+
+    }
+}
