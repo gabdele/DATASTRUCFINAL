30fb4ff4906921da654a0b062c766ce2f711700d
Gabriela de Leon <gldeleon@Gabrielas-MacBook-Pro.local>
2024-04-27 18:22:16 -0400

    window testing added

--- src/Menu.java
+++ src/Menu.java
@@ -1,5 +1,6 @@
 public class Menu {
     public static void main(String[] args) {
-
+        QuestionMakerWindow thiswindow = new QuestionMakerWindow();
+        thiswindow.setVisible(true);
     }
 }
--- src/QuestionMakerWindow.java
+++ src/QuestionMakerWindow.java
@@ -10,10 +10,15 @@ public class QuestionMakerWindow extends JFrame { //should be going recursively,
     String[] listOfTypes = {"Dropdown","Checkbox","Radio Buttons"};
 
     QuestionMakerWindow(){
+        setSize(600,600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         ID++;
         heading = new JLabel("Question "+ID);
         whatKind = new JLabel("what kind of options should be given?");
         thisKind = new JComboBox(listOfTypes);
+        add(heading);
+        add(whatKind);
+        add(thisKind);
 
     }
 }
