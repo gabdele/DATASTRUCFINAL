0d6e602c4a3238875eed33b8c17149778aff5713
Gabriela de Leon <gldeleon@Gabrielas-MacBook-Pro.local>
2024-04-28 13:44:20 -0400

    constructing QMW2

--- src/Menu.java
+++ src/Menu.java
@@ -1,6 +1,12 @@
-public class Menu {
+import javax.swing.*;
+public class Menu extends JFrame {
+
+    Menu(){
+
+    }
     public static void main(String[] args) {
         QuestionMakerWindow thiswindow = new QuestionMakerWindow();
         thiswindow.setVisible(true);
     }
+
 }
--- src/QuestionMakerWindow2.java
+++ src/QuestionMakerWindow2.java
@@ -1,4 +1,9 @@
+import java.awt.*;
+
 public class QuestionMakerWindow2 extends QuestionMakerWindow{
 Question ques = QuestionMakerWindow.getQues();
+QuestionMakerWindow2(){
+
+    }
 
 }
