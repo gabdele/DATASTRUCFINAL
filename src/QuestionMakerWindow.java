ee2c079cc127b9a122a6ecd841dce9d25190dce6
Gabriela de Leon <gldeleon@Gabrielas-MacBook-Pro.local>
2024-04-27 22:05:14 -0400

    makerwindow basics

--- src/QuestionMakerWindow.java
+++ src/QuestionMakerWindow.java
@@ -1,24 +1,44 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class QuestionMakerWindow extends JFrame { //should be going recursively, with ID increasing somehow
+public class QuestionMakerWindow extends JFrame implements ActionListener { //should be going recursively, with ID increasing somehow
     JLabel heading;
     JLabel whatKind;
     JComboBox thisKind;
     JLabel howMany;
+    JTextField thismany;
     JButton next;
     int ID = 0;
     String[] listOfTypes = {"Dropdown","Checkbox","Radio Buttons"};
 
     QuestionMakerWindow(){
-        setSize(600,600);
+        setSize(400,300);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         ID++;
-        heading = new JLabel("Question "+ID);
-        whatKind = new JLabel("what kind of options should be given?");
+        heading = new JLabel(" Question "+ID);
+        whatKind = new JLabel(" What kind of options should be given?");
         thisKind = new JComboBox(listOfTypes);
+        howMany = new JLabel("how many options?");
+        thismany = new JTextField(3);
+        next = new JButton("next");
+        next.addActionListener(this);
+
+        heading.setFont(new Font("PLAIN", Font.BOLD,30));
+        whatKind.setFont(new Font("PLAIN",Font.PLAIN,20));
+
+        setLayout(new GridLayout(3,1));
         add(heading);
         add(whatKind);
         add(thisKind);
+        add(howMany);
+        add(thismany);
+
+
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
 
     }
 }
